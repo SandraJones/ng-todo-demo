272e20b8d753199e9e70020526b9f9bb0be14f40
Sandy Jones <sandynashv@gmail.com>
2016-05-18 21:02:21 -0500

    completed setup

--- app/app.js
+++ app/app.js
@@ -1,1 +1,6 @@
-console.log("hello");
+//place for modules or plugins in parentheses
+var app = angular.module("TodoApp", []);
+
+app.controller("TodoCtrl", function($scope) {
+	$scope.welcome = "Hello";
+})
