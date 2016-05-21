8c3cb34504afeb9f52af9a83c68e2a371ece1df5
Sandy Jones <sandynashv@gmail.com>
2016-05-21 10:12:17 -0500

    set buttons

--- app/app.js
+++ app/app.js
@@ -2,9 +2,21 @@
 var app = angular.module("TodoApp", []);
 
 app.controller("NavCtrl", function($scope ){
-	$scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}]
+	$scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}];
 });
 
 app.controller("TodoCtrl", function($scope) {
 	$scope.welcome = "Hello";
-})
+	$scope.showListView = true;
+
+	$scope.newItem= function(){
+		console.log("newItem");
+		$scope.showListView = false;
+	};
+	$scope.allItem = function(){
+		console.log("allItem");
+		$scope.showListView = true;
+	};
+
+
+});
