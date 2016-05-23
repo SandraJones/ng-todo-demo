90f72e62045f41c05c6d9550cbc42027ea4fb149
Sandy Jones <sandynashv@gmail.com>
2016-05-23 18:25:18 -0500

    added json and removed obj from controllers

--- app/app.js
+++ app/app.js
@@ -4,9 +4,11 @@ app.config(function($routeProvider){
 	$routeProvider.when("/items/list", {
 		templateUrl: "partials/item-list.html",
 		controller:"ItemListCtrl"
+
 	}).when("/items/new", {
 		templateUrl: "partials/item-new.html",
 		controller: "ItemNewCtrl"
+		
 	}).when("/items/details", {
 		templateUrl: "partials/item-details.html",
 		controller: "ItemViewCtrl"
