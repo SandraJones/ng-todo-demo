c308cfbe6495b126f2343a5c254e29a666e604fb
Sandy Jones <sandynashv@gmail.com>
2016-05-23 19:51:28 -0500

    firebase working

--- app/controllers/ItemListCrtl.js
+++ app/controllers/ItemListCrtl.js
@@ -1,3 +1,29 @@
-app.controller("ItemListCtrl", function($scope) {
+app.controller("ItemListCtrl", function($scope, $http, $location) {
 	$scope.items = [];
+	//use similar syntax with the post and other methods
+	var getItems=function(){
+		$http.get("https://todo-appskj.firebaseio.com/items.json")
+		   .success(function(itemObject) {
+		   	var itemCollection = itemObject;
+		   	console.log("itemObject", itemObject);
+		   	Object.keys(itemCollection).forEach(function(key){ //returns an array of all the keys in the object, then it goes thru every key in that array and putting keyinto the funciton stores that value
+		   		//give the obj at this spot an id property
+		   		itemCollection[key].id=key;//adding an id=key is the firebase id
+		   		//pushing the object into the items array
+		   		$scope.items.push(itemCollection[key]);
+		   	})
+		   });
+	 }
+	 getItems();
+		// it is only ./data because it gets called from index.html so we are in the root
+		// <body ng-app = "TodoApp"> ties the app to the html
+		$scope.itemDelete = function(itemId){
+			console.log("itemId", itemId);
+			$http
+				.delete(`https://todo-appskj.firebaseio.com/items/${itemId}.json`)
+				.success(function(response){
+					console.log(response);
+					getItems();
+				});
+		}
 });
--- app/controllers/ItemNewCrtl.js
+++ app/controllers/ItemNewCrtl.js
@@ -1,13 +1,33 @@
-app.controller("ItemNewCtrl", function($scope) {
-	$scope.newTask = {};
-	$scope.items = [];
+app.controller("ItemNewCtrl", function($scope, $http, $location) {
+	$scope.newTask = {
+		//want to add in the properties that I'm going to need
+		//we want our firebase data to all have the same structure
+		//we will assign empty strings to everything to catch user error
+		assignedTo: "",
+		dueDate: "",
+		dependencies: "",
+		isCompleted: false,
+		location: "",
+		task: "",
+		urgency: ""
+	};
 
 	$scope.addNewItem = function() {
-		console.log("you added a new item", $scope.newTask);
-		//this is where a send to firebase will go 
-		$scope.newTask.isCompleted = false;
-		$scope.newTask.id= $scope.items.length;
-		$scope.items.push($scope.newTask);
-		$scope.newTask = "";
-	};
-	});
+		$http.post(
+			"https://todo-appskj.firebaseio.com/items.json", 
+			JSON.stringify({
+				assignedTo: $scope.newTask.assignedTo,
+				dueDate: $scope.newTask.dueDate,
+				dependencies: $scope.newTask.dependencies,
+				isCompleted: $scope.newTask.isCompleted,
+				location: $scope.newTask.location,
+				task: $scope.newTask.task,
+				urgency: $scope.newTask.urgency
+		})
+	)
+	.success(function(response){
+		console.log(response);
+		$location.url("/items/list")//make sure it matches what we have in app.js
+	});	
+};
+});
