0c100a024fd71d6711dd60ad541ea01efbe6ff6e
Sandy Jones <sandynashv@gmail.com>
2016-05-26 17:39:54 -0500

    spacing

--- app/factories/ItemFactory.js
+++ app/factories/ItemFactory.js
@@ -1,7 +1,5 @@
 "use strict";
-app.factory("itemStorage", function($q, $http, firebaseURL){  //order of q and http here do not matter if I do it this way
-	//q handles promises for angular; q is angular's version of promises and http is like xhr
-	//"itemStorage" is the name your project knows your factory by
+app.factory("itemStorage", function($q, $http, firebaseURL){  
 
 	var getItemList=function(){  //the whole todo or chore
 	var items = [];
@@ -20,22 +18,20 @@ app.factory("itemStorage", function($q, $http, firebaseURL){  //order of q and h
 					})
 	   		.error(function(error){		
 	   		  reject(error);
-	   });
-		  })
-	 }
+	   		});
+		})
+	}
 
-
-
-var deleteItem = function(itemId) {
-	return $q(function(resolve, reject){
-			$http
-				.delete(firebaseURL + "items/" + itemId + ".json")
-				.success(function(objectFromFirebase){
-					resolve(objectFromFirebase)
-				})
-				//delete url has to be concatenated because we are passing in the itemId
-	})
-}
+	var deleteItem = function(itemId) {
+		return $q(function(resolve, reject){
+				$http
+					.delete(firebaseURL + "items/" + itemId + ".json")
+					.success(function(objectFromFirebase){
+						resolve(objectFromFirebase)
+					})
+					//delete url has to be concatenated because we are passing in the itemId
+		})
+	}
 
 	var postNewItem= function(newItem){
 		return $q(function(resolve, reject){
@@ -60,64 +56,69 @@ var deleteItem = function(itemId) {
 		});
 	}
 
-		var getSingleItem = function(itemId){	
-					return $q(function(resolve, reject){
-					$http.get(firebaseURL + "items/" + itemId + ".json")
-					   .success(function(itemObject) {
-						  	resolve(itemObject);
-						   	})
-				   		.error(function(error){		
-				   		  reject(error);
-				   });
-				})		
+	var getSingleItem = function(itemId){	
+				return $q(function(resolve, reject){
+				$http.get(firebaseURL + "items/" + itemId + ".json")
+				   .success(function(itemObject) {
+					  	resolve(itemObject);
+					   	})
+			   		.error(function(error){		
+			   		  reject(error);
+			   });
+			})		
 	}
 
-	 var updateItem = function(itemId, newItem){
-        return $q(function(resolve, reject) {
-            $http.put(
-                firebaseURL + "items/" + itemId + ".json",
-                JSON.stringify({
-                    assignedTo: newItem.assignedTo,
-                    dependencies: newItem.dependencies,
-                    dueDate: newItem.dueDate,
-                    isCompleted: newItem.isCompleted,
-                    location: newItem.location,
-                    task: newItem.task,
-                    urgency: newItem.urgency
-                })
-            )
-            .success(
-                function(objectFromFirebase) {
-                    resolve(objectFromFirebase);
-                }
-            );
+	var updateItem = function(itemId, newItem){
+     return $q(function(resolve, reject) {
+        $http.put(
+          firebaseURL + "items/" + itemId + ".json",
+            JSON.stringify({
+                assignedTo: newItem.assignedTo,
+                dependencies: newItem.dependencies,
+                dueDate: newItem.dueDate,
+                isCompleted: newItem.isCompleted,
+                location: newItem.location,
+                task: newItem.task,
+                urgency: newItem.urgency
+            })
+         )
+          .success(
+              function(objectFromFirebase) {
+                  resolve(objectFromFirebase);
+              }
+          );
         });
-    };
+  };
 
-     var updateCompletedStatus = function(newItem){
-        return $q(function(resolve, reject) {
-            $http.put(
-                firebaseURL + "items/" + newItem.id + ".json",
-                JSON.stringify({
-                    assignedTo: newItem.assignedTo,
-                    dependencies: newItem.dependencies,
-                    dueDate: newItem.dueDate,
-                    isCompleted: newItem.isCompleted,
-                    location: newItem.location,
-                    task: newItem.task,
-                    urgency: newItem.urgency
-                })
-            )
-            .success(
-                function(objectFromFirebase) {
-                    resolve(objectFromFirebase);
-                }
-            );
-        });
-    };
-
+  var updateCompletedStatus = function(newItem){
+      return $q(function(resolve, reject) {
+          $http.put(
+              firebaseURL + "items/" + newItem.id + ".json",
+              JSON.stringify({
+                  assignedTo: newItem.assignedTo,
+                  dependencies: newItem.dependencies,
+                  dueDate: newItem.dueDate,
+                  isCompleted: newItem.isCompleted,
+                  location: newItem.location,
+                  task: newItem.task,
+                  urgency: newItem.urgency
+              })
+          )
+          .success(
+              function(objectFromFirebase) {
+                  resolve(objectFromFirebase);
+              }
+          );
+      });
+  };
 
      //key and a function, key and a function, key and a function
-	 return {getItemList:getItemList, updateCompletedStatus:updateCompletedStatus, updateItem:updateItem, getSingleItem:getSingleItem, deleteItem:deleteItem, postNewItem:postNewItem}
+	return {getItemList:getItemList, updateCompletedStatus:updateCompletedStatus, 
+	 	updateItem:updateItem, getSingleItem:getSingleItem, deleteItem:deleteItem, 
+	 	postNewItem:postNewItem}
 //way to call from any file, have access because in app.controller itemStorage is passed in so we can use these functions everywhere
 });
+
+// line 1 //order of q and http here do not matter if I do it this way
+	//q handles promises for angular; q is angular's version of promises and http is like xhr
+	//"itemStorage" is the name your project knows your factory by
